pitch and roll errors are fixed (angles are in degrees and not radians)

controller.py
@@ -20,79 +20,9 @@ import matplotlib.animation as animation
 from initialize import initState, dt
 from AnimateSingleUav import PlotandAnimate
 import time
+np.set_printoptions(linewidth=np.inf)
+np.set_printoptions(suppress=True)
 
-def main():
-    # Initialize an instance of a uav dynamic model with:
-    # dt: time interval
-    # initState: initial state
-    # set it as 1 tick: i.e: 1 ms
-    uav1 = uav.UavModel(dt, initState)
-    # Upload the traj in csv file format
-    # rows: time, xdes, ydes, zdes
-    filename = "trajectoriescsv/lin.csv"
-    timeStamped_traj = np.genfromtxt(filename, delimiter=',')
-    # final time of traj in ms
-    tf_ms = timeStamped_traj[0,-1]*1e3
-    
-    #initialize the controller and allocate current state (both sensor and state are the state)
-    # This is kind of odd and should be part of state
-    control, setpoint, sensors, state = initController()
-    print("initial values: \n")
-    print("initial thrust: %d\n",control.thrustSI)
-    print("initial torque: ",control.torque[0],control.torque[1], control.torque[2])
-    print("initial position", state.position.x,state.position.y,state.position.z)
-    
-    # Note that 1 tick == 1ms
-    # note that the attitude controller will only compute a new output at 500 Hz
-    # and the position controller only at 100 Hz
-    # If you want an output always, simply select tick==0
-    full_state = np.zeros((1,13))
-    
-    for tick in range(0, int(tf_ms)+1):
-        # update desired state
-        setpoint  = updateDesState(tick, setpoint, timeStamped_traj[1::,tick])
-        # update current state
-        state     = updateState(state, uav1.state)
-        sensors   = updateSensor(sensors, uav1.state)
-        # query the controller
-        cffirmware.controllerSJC(control, setpoint, sensors, state, tick)
-        control_inp =  np.array([control.thrustSI, control.torque[0], control.torque[1], control.torque[2]])
-        uav1.states_evolution(control_inp)
-
-        print("thrust: %d\n",control.thrustSI)
-        print("torque: ",control.torque[0],control.torque[1], control.torque[2])
-        print("position", state.position.x,state.position.y,state.position.z)
-        print("rpy", state.attitude.roll,state.attitude.pitch,state.attitude.yaw)
-    
-        full_state = np.concatenate((full_state, uav1.state.reshape(1,13)))
-        
-
-    full_state = np.delete(full_state, 0, 0)
-    fig     = plt.figure(figsize=(10,10))
-    ax      = fig.add_subplot(autoscale_on=True,projection="3d")
-    sample  = 50
-    animate = PlotandAnimate(fig, ax, uav1, full_state[::sample,:])
-
-    animateAndSave = True
-    if animateAndSave:
-        videoname  = 'hover.mp4' 
-        dt_sampled = dt * sample
-        show       = False
-        save       = True
-        if show:
-            print("Showing animation.")
-        if save:
-            print("Converting Animation to Video. \nPlease wait...")
-        now = time.time()
-        startanimation = animate.startAnimation(videoname,show,save,dt_sampled)
-        # plotfulltraj = animate.plotFulltraj();
-        end = time.time()
-        print("Run time:  {:.3f}s".format((end - now)))
-    else:
-        print("plotting full trajectory")
-        plotfulltraj = animate.plotFulltraj();
-##----------------------------------------------------------------------------------------------------------------------------------------------------------------##        
-##----------------------------------------------------------------------------------------------------------------------------------------------------------------##
 def initController():
     cffirmware.controllerSJCInit()
     # Allocate output variable
@@ -114,7 +44,8 @@ def setTrajmode(setpoint):
     setpoint.mode.yaw = cffirmware.modeDisable
     return setpoint
 
-def updateDesState(tick, setpoint, fulltraj):
+def updateDesState(setpoint, fulltraj):
+    # fulltraj[3::] = 0
     setpoint.position.x = fulltraj[0]  # m
     setpoint.position.y = fulltraj[1]  # m
     setpoint.position.z = fulltraj[2]  # m
@@ -124,14 +55,15 @@ def updateDesState(tick, setpoint, fulltraj):
     setpoint.acceleration.x = fulltraj[6]  # m/s^2
     setpoint.acceleration.y = fulltraj[7]  # m/s^2
     setpoint.acceleration.z = fulltraj[8]  # m/s^2
-    setpoint.attitude.yaw = 0  # deg
+    # setpoint.attitude.yaw = 0  # deg
     return setpoint
-
+    
 def updateSensor(sensors,uavState):
-    sensors.gyro.x = uavState[10] # deg/s
-    sensors.gyro.y = uavState[11] # deg/s
-    sensors.gyro.z = uavState[12] # deg/s
+    sensors.gyro.x = np.degrees(uavState[10]) # deg/s
+    sensors.gyro.y = np.degrees(uavState[11]) # deg/s
+    sensors.gyro.z = np.degrees(uavState[12]) # deg/s
     return sensors
+
 
 def updateState(state, uavState):
     state.position.x = uavState[0]   # m
@@ -141,13 +73,93 @@ def updateState(state, uavState):
     state.velocity.y = uavState[4]    # m/s
     state.velocity.z = uavState[5]    # m/s
     q_curr = np.array(uavState[6:10]).reshape((4,))
-    rpy_state  = to_euler(q_curr,convention='zyx')
-    state.attitude.roll  = rpy_state[0]
-    state.attitude.pitch = rpy_state[1]
-    state.attitude.yaw   = rpy_state[2]
-    return state
+    rpy_state  = to_euler(q_curr,convention='xyz')
+    state.attitude.roll  = np.degrees(rpy_state[0])
+    state.attitude.pitch = np.degrees(-rpy_state[1])
+    state.attitude.yaw   = np.degrees(rpy_state[2])
+    fullState = np.array([state.position.x,state.position.y,state.position.z, 
+                          state.velocity.x,state.velocity.y, state.velocity.z, 
+                          q_curr[0],q_curr[1],q_curr[2],q_curr[3], 0,0,0]).reshape((13,))
+    return state,fullState
+
+def stateUavStateError(state, uavState):    
+    state_ = np.array([state.position.x, state.position.y, state.position.z, state.attitude.roll, state.attitude.pitch, state.attitude.yaw])
+    q_curr = np.array(uavState[6:10]).reshape((4,))
+    rpy_state  = to_euler(q_curr,convention='xyz')
+    uavState_ = np.array([uavState[0], uavState[1], uavState[2],rpy_state[0], rpy_state[1], rpy_state[2]])
+    return state_ - uavState_
 
 
+##----------------------------------------------------------------------------------------------------------------------------------------------------------------##        
+##----------------------------------------------------------------------------------------------------------------------------------------------------------------##
+def main():
+    # Initialize an instance of a uav dynamic model with:
+    # dt: time interval
+    # initState: initial state
+    # set it as 1 tick: i.e: 1 ms
+    uav1 = uav.UavModel(dt, initState)
+    # Upload the traj in csv file format
+    # rows: time, xdes, ydes, zdes
+    filename = "trajectoriescsv/inf.csv"
+    timeStamped_traj = np.genfromtxt(filename, delimiter=',')
+    # final time of traj in ms
+    tf_ms = timeStamped_traj[0,-1]*1e3
+    # Simulation time
+    tf_sim = tf_ms + 5e3
+    #initialize the controller and allocate current state (both sensor and state are the state)
+    # This is kind of odd and should be part of state
+    control, setpoint, sensors, state = initController()
+    # Note that 1 tick == 1ms
+    # note that the attitude controller will only compute a new output at 500 Hz
+    # and the position controller only at 100 Hz
+    # If you want an output always, simply select tick==0
+    full_state = np.zeros((1,13))
+    ref_state = np.zeros((1 ,3))
+    for tick in range(0, int(tf_sim)+1):
+        # update desired state
+        if tick <= int(tf_ms):
+            setpoint  = updateDesState(setpoint, timeStamped_traj[1::,tick])
+            ref_state  = np.concatenate((ref_state, timeStamped_traj[1:4,tick].reshape((1,3))))
+        else:
+            setpoint = updateDesState(setpoint, timeStamped_traj[1::,-1])
+            ref_state  = np.concatenate((ref_state, timeStamped_traj[1:4,-1].reshape((1,3))))
+        # update current state
+        state,fullState = updateState(state, uav1.state)
+        sensors         = updateSensor(sensors, uav1.state)
+        # query the controller
+        cffirmware.controllerSJC(control, setpoint, sensors, state, tick)
+        # states evolution
+        control_inp =  np.array([control.thrustSI, control.torque[0], control.torque[1], control.torque[2]])
+        uav1.states_evolution(control_inp)
+        print(control_inp)
+        full_state = np.concatenate((full_state, uav1.state.reshape(1,13)))
+
+    # Animation    
+    full_state = np.delete(full_state, 0, 0)
+    ref_state  = np.delete(ref_state, 0, 0)
+    fig     = plt.figure(figsize=(10,10))
+    ax      = fig.add_subplot(autoscale_on=True,projection="3d")
+    sample  = 100
+    animate = PlotandAnimate(fig, ax, uav1, full_state[::sample,:],ref_state[::sample,:])
+
+    animateAndSave = True
+    if animateAndSave:
+        videoname  = 'infinty8_newmass.mp4' 
+        dt_sampled = dt * sample
+        show       = False
+        save       = True
+        if show:
+            print("Showing animation.")
+        if save:
+            print("Converting Animation to Video. \nPlease wait...")
+        now = time.time()
+        startanimation = animate.startAnimation(videoname,show,save,dt_sampled)
+        # plotfulltraj = animate.plotFulltraj();
+        end = time.time()
+        print("Run time:  {:.3f}s".format((end - now)))
+    else:
+        print("plotting full trajectory")
+        plotfulltraj = animate.plotFulltraj();    
 if __name__ == '__main__':
 	main()
 

genTrajectory/trajectoryGen.py
@@ -13,7 +13,7 @@ np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(suppress=True)
 
 # Problem data.
-type_traj = 'lin'
+type_traj = 'inf'
 if type_traj in 'helix':
     ## WayPoints for cirle trajectory
     r = 1
@@ -31,7 +31,7 @@ if type_traj in 'helix':
 elif(type_traj in 'inf'):
     ## Waypoints for infinity trajectory
     height = 0.7
-    w = 0.2 * np.pi
+    w = 0.35* 0.2 * np.pi
     T = (2*np.pi)/w
     time = np.arange(0,T,1e-3)
     print('dt = ',time[1]-time[0])
@@ -40,16 +40,17 @@ elif(type_traj in 'inf'):
     data[1,:]  =  np.sin(w*time)
     data[2,:]  =  np.sin(2*w*time)
     data[3,:]  =  height
-    pieces = 25
+    pieces = 30
 else:
-    x0 = 0.7
-    xf = 3
-    T = 8
+    x0 = 0
+    xf = 0.5
+    T = 10
     time = np.arange(0,T,1e-3)
     data = np.empty((4,time.size))
     dt = 1e-3
     data[0,:] = time
-    data[3,:] = x0 + ((time/time[-1])*(xf - x0))
+    data[1,:] = x0 + ((time/time[-1])*(xf - x0))
+    data[3,:] = 0.7
     pieces = 20 
 ################################################################################################################################################
 ## Define the number of splines
@@ -232,7 +233,7 @@ postraj  = np.zeros((4,step*pieces))
 veltraj  = np.zeros((4,step*pieces))
 acctraj  = np.zeros((4,step*pieces))
 jerktraj = np.zeros((4,step*pieces))
-fulltraj = np.zeros((12,step*pieces))
+fulltraj = np.zeros((10,step*pieces))
 
 postraj[0,:]  = time[:(step*pieces)]
 veltraj[0,:]  = time[:(step*pieces)]
@@ -256,8 +257,11 @@ for i in range(0,n,8):
     veltraj[1:,stepInd:stepInd+step]  = np.array([vxpoly(time_hk),vypoly(time_hk),vzpoly(time_hk)]).reshape(3,len(time_hk))
     acctraj[1:,stepInd:stepInd+step]  = np.array([axpoly(time_hk),aypoly(time_hk),azpoly(time_hk)]).reshape(3,len(time_hk))
     jerktraj[1:,stepInd:stepInd+step] = np.array([jxpoly(time_hk),jypoly(time_hk),jzpoly(time_hk)]).reshape(3,len(time_hk))
+
     fulltraj[1:4,stepInd:stepInd+step]  = np.array([xpoly(time_hk),ypoly(time_hk),zpoly(time_hk)]).reshape(3,len(time_hk))
+
     fulltraj[4:7,stepInd:stepInd+step]  = np.array([vxpoly(time_hk),vypoly(time_hk),vzpoly(time_hk)]).reshape(3,len(time_hk))
+
     fulltraj[7:10,stepInd:stepInd+step] = np.array([axpoly(time_hk),aypoly(time_hk),azpoly(time_hk)]).reshape(3,len(time_hk))
     stepInd += step
 

initialize.py
@@ -2,7 +2,7 @@ import numpy as np
 from rowan import from_matrix, to_matrix 
 from utils import Rx, Ry, Rz
 
-dt = 0.002
+dt = 1e-3
 initPos = np.array([0,0,0.7])
 initR = np.eye(3)
 # initialize Rotation matrix about Roll-Pitch-Yaw

simulator/AnimateSingleUav.py
@@ -9,16 +9,17 @@ import uav
 
 
 class PlotandAnimate:
-    def __init__(self, fig, ax, uavModel,full_state): #,reference_state): 
+    def __init__(self, fig, ax, uavModel,full_state, reference_state): 
         # Initialize the Actual and Reference states
         self.full_state      = full_state
-        # self.reference_state = reference_state
+        self.reference_state = reference_state
         self.uavModel        = uavModel
         # Initialize a 3d figure
         self.fig = fig#plt.figure(figsize=(10,10))
         self.ax  = ax#self.fig.add_subplot(autoscale_on=True,projection="3d")
         # self.ax.view_init(25, 35)
-        self.ax.view_init(25, -(90+35))
+        # self.ax.view_init(25, -(90+35))
+        self.ax.view_init(25,35)
         # Create the lines and vectors to draw body and desired frames
         self.line, = self.ax.plot(self.full_state[0,0:1], self.full_state[1,0:1], self.full_state[2,0:1], 'b--', lw=1)
         self.vec1  = self.ax.quiver([],[],[],[],[],[])
@@ -38,7 +39,7 @@ class PlotandAnimate:
         if show:   
             plt.show()
         if save:
-            self.ani.save(videoname)
+            self.ani.save('Videos/'+videoname)
 
 
     def plotFulltraj(self):
@@ -46,8 +47,14 @@ class PlotandAnimate:
         x = self.full_state[:,0]
         y = self.full_state[:,1]
         z = self.full_state[:,2]
-       
-        self.ax.plot3D(x, y, z, 'b--',lw=1.5)
+        xref = self.reference_state[:,0]
+        yref = self.reference_state[:,1]
+        zref = self.reference_state[:,2]
+        self.ax.plot3D(x, y, z, 'k--',lw=1.5)
+        self.ax.plot3D(xref, yref ,zref,'g--',lw=1.5)
+        ud = np.array([1,0,0])
+        vd = np.array([0,1,0])  
+        wd = np.array([0,0,1])
         for i in range(0,len(x)):
             q = self.full_state[i,6:10]
             R_i = to_matrix(q)
@@ -57,14 +64,22 @@ class PlotandAnimate:
             self.vec1  = self.ax.quiver(x[i],y[i],z[i], u[0], u[1] ,u[2],color = 'r', length = 0.2)
             self.vec2  = self.ax.quiver(x[i],y[i],z[i], v[0], v[1] ,v[2],color = 'g', length = 0.2)  
             self.vec3  = self.ax.quiver(x[i],y[i],z[i], w[0], w[1] ,w[2],color = 'b', length = 0.2)
+           
+            self.vec1  = self.ax.quiver(x,y,z, u[0], u[1] ,u[2],color = 'r', length = 0.2)
+            self.vec2  = self.ax.quiver(x,y,z, v[0], v[1] ,v[2],color = 'g', length = 0.2)
+            self.vec3  = self.ax.quiver(x,y,z, w[0], w[1] ,w[2],color = 'b', length = 0.2)
+            self.vec1r = self.ax.quiver(xref,yref,zref, ud[0], ud[1] ,ud[2],color = 'r', length = 0.3)
+            self.vec2r = self.ax.quiver(xref,yref,zref, vd[0], vd[1] ,vd[2],color = 'g', length = 0.3)
+            self.vec3r = self.ax.quiver(xref,yref,zref, wd[0], wd[1] ,wd[2],color = 'b', length = 0.3)
         self.ax.plot3D(x, y, z, 'k--',lw=1.5)    
         plt.show()
 
     def setlimits(self):
+        edge  = 0.9
         max_x = max(self.full_state[0,:])
         max_y = max(self.full_state[1,:])
         max_z = max(self.full_state[2,:])
-        edge  = 0.5
+       
         self.ax.set_xlim3d([-max_x-edge, max_x+edge])
         self.ax.set_xlabel('X')
         self.ax.set_ylim3d([-max_y-edge, max_y+edge])
@@ -72,21 +87,20 @@ class PlotandAnimate:
         self.ax.set_zlim3d([-max_z-edge, max_z+edge]) #
         self.ax.set_zlabel('Z')
 
-    def drawQuivers(self, x, y, z, q):#, xref, yref, zref, qref):
+    def drawQuivers(self, x, y, z, q, xref, yref, zref):
         R_i = to_matrix(q)
         u = R_i[:,0]
         v = R_i[:,1]
         w = R_i[:,2]
-        # Rd_i = to_matrix(qref)
-        # ud = Rd_i[:,0]
-        # vd = Rd_i[:,1]
-        # wd = Rd_i[:,2]
+        ud = np.array([1,0,0])
+        vd = np.array([0,1,0])
+        wd = np.array([0,0,1])
         self.vec1  = self.ax.quiver(x,y,z, u[0], u[1] ,u[2],color = 'r', length = 0.2)
         self.vec2  = self.ax.quiver(x,y,z, v[0], v[1] ,v[2],color = 'g', length = 0.2)
         self.vec3  = self.ax.quiver(x,y,z, w[0], w[1] ,w[2],color = 'b', length = 0.2)
-        # self.vec1r = self.ax.quiver(xref,yref,zref, ud[0], ud[1] ,ud[2],color = 'r', length = 0.5)
-        # self.vec2r = self.ax.quiver(xref,yref,zref, vd[0], vd[1] ,vd[2],color = 'g', length = 0.5)
-        # self.vec3r = self.ax.quiver(xref,yref,zref, wd[0], wd[1] ,wd[2],color = 'b', length = 0.5)
+        self.vec1r = self.ax.quiver(xref,yref,zref, ud[0], ud[1] ,ud[2],color = 'r', length = 0.5)
+        self.vec2r = self.ax.quiver(xref,yref,zref, vd[0], vd[1] ,vd[2],color = 'g', length = 0.5)
+        self.vec3r = self.ax.quiver(xref,yref,zref, wd[0], wd[1] ,wd[2],color = 'b', length = 0.5)
         
 
     def getCurrState(self, i):
@@ -100,8 +114,7 @@ class PlotandAnimate:
         xref = self.reference_state[:i+1,0]
         yref = self.reference_state[:i+1,1]
         zref = self.reference_state[:i+1,2]
-        qref = self.reference_state[i,6:10].reshape(4,)
-        return xref, yref, zref, qref
+        return xref, yref, zref
 
     def getArmpos(self, x, y, z, q):
         R_i      = to_matrix(q)
@@ -112,9 +125,10 @@ class PlotandAnimate:
         _armI2   = position + R_i @ (self._armb2.reshape(3,))
         return armI1, armI2, _armI1, _armI2
 
-    def drawActvsRefTraj(self, x, y, z):#, xref, yref, zref):
-            self.ax.plot3D(x, y, z, 'b--',lw=1.5)
-            # self.ax.plot3D(xref, yref ,zref,'g--',lw=2)
+    def drawActvsRefTraj(self, x, y, z, xref, yref, zref):
+            self.ax.plot3D(x, y, z, 'k-.',lw=1.5,label="Actual Trajectory")
+            self.ax.plot3D(xref, yref ,zref,'g--',lw=1.5,label="Reference Trajectory")
+            self.ax.legend()
 
     def drawQuadrotorArms(self, x, y, z, armI1, armI2, _armI1, _armI2):
         self.ax.plot3D(np.linspace(x, armI1[0]), np.linspace(y, armI1[1]), np.linspace(z, armI1[2]),'k',lw=2)
@@ -133,11 +147,11 @@ class PlotandAnimate:
         self.ax.cla()
         self.setlimits()
         x, y, z, q                   = self.getCurrState(i)
-        # xref,yref,zref, qref         = self.getRefState(i) 
+        xref,yref,zref               = self.getRefState(i) 
         armI1, armI2, _armI1, _armI2 = self.getArmpos(x[i],y[i],z[i],q)
 
-        self.drawQuivers(x[i],y[i],z[i], q)#, xref[i], yref[i], zref[i], qref)
-        self.drawActvsRefTraj(x, y, z)# xref, yref, zref)
+        self.drawQuivers(x[i],y[i],z[i], q, xref[i], yref[i], zref[i])
+        self.drawActvsRefTraj(x, y, z, xref, yref, zref)
         self.drawQuadrotorArms(x[i], y[i], z[i], armI1, armI2, _armI1, _armI2)
 
         Xb,Yb,Zb = RotatedCylinder(0,0,0.1,0.1,q) 

uavDy/uav.py
@@ -1,6 +1,6 @@
 import numpy as np
 from rowan.calculus import integrate as quat_integrate
-from rowan import from_matrix, to_matrix, to_euler
+from rowan import from_matrix, to_matrix, to_euler, from_euler
 from scipy import  integrate, linalg
 from utils import skew
 
@@ -13,7 +13,7 @@ class UavModel:
                0.718277 1.800197 29.261652)*10^-6 [kg.m^2]"""
 
         def __init__(self,dt ,state):
-            self.m      = 0.034  
+            self.m      = 0.028 
             self.I      = np.array([[16.571710, 0, 0],[0, 16.655602, 0],[0, 0, 29.261652]])* 1e-6 
             self.invI   = linalg.inv(self.I)
             self.d      = 4e-3